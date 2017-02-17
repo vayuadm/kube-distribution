4b89d70e7f9fa6f2fa81f33ae3d4896a2a029089
Effi <effoeffi@gmail.com>
2017-02-17 14:20:03 +0200

    fix test;

--- set_test.go
+++ set_test.go
@@ -28,13 +28,10 @@ func TestSet_NotContains(t *testing.T) {
 func TestSet_ToArray(t *testing.T) {
 
 	const x1 = "hello"
-	const x2 = "world"
 
 	s := NewSet()
 	s.Add(x1)
-	s.Add(x2)
 	arr := s.ToArray()
 
 	assert.Equal(t, x1, arr[0])
-	assert.Equal(t, x2, arr[1])
 }
