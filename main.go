7b7eb5a7394dac9250295aa27ccea45e78a4b59b
Effi <effoeffi@gmail.com>
2017-02-17 14:32:56 +0200

    watch branches; parse image tag; update deployments;

--- main.go
+++ main.go
@@ -2,9 +2,23 @@ package main
 
 import (
 	"net/http"
+	"fmt"
+	"strings"
+	"errors"
+	"os"
+
+	log "github.com/Sirupsen/logrus"
+)
+
+var (
+	kube KubeClient
+	watchBranches Set
 )
 
 func main() {
+
+	kube = NewKubeClient()
+	watchBranches = getWatchRepositories()
 
 	http.HandleFunc("/events", handler)
 	http.ListenAndServe(":5050", nil)
@@ -12,5 +26,54 @@ func main() {
 
 func handler(writer http.ResponseWriter, request *http.Request) {
 
-	writer.WriteHeader(http.StatusOK)
+	if repositories, err := GetPushEventRepositories(request.Body); err != nil {
+		log.Error(err)
+		writer.WriteHeader(http.StatusBadRequest)
+	} else {
+		for _, currRepository := range repositories {
+			branch, namespace, deployment, version, err := parseTag(currRepository.Tag)
+			if err != nil {
+				log.Error(err)
+				continue
+			}
+
+			fmt.Println("branch: ", branch, " ur:", watchBranches)
+			if watchBranches.Contains(branch) {
+				image := fmt.Sprintf("%s:%s", currRepository.Name, version)
+				if err := kube.UpdateDeployment(deployment, namespace, image); err != nil {
+					log.Errorf("Failed to update deployment: %s, branch: %s, namespace: %s, image: %s",
+						deployment, branch, namespace, image, err)
+				}
+			}
+		}
+		writer.WriteHeader(http.StatusOK)
+	}
+}
+
+// example tag: master-default-ceribrouideplyment-7
+func parseTag(tag string) (branch, namespace, deployment, version string, err error) {
+
+	ret := strings.Split(tag, "-")
+	if len(ret) != 4 || ret[0] == "" || ret[1] == "" || ret[2] == "" || ret[3] == "" {
+		return "", "", "", "", errors.New(fmt.Sprintf(
+			"Failed to parse docker image tag: %s. Format should be: <branch>-<kubernetes namespace>-<kubernetes deployment name>-<version>", tag))
+	}
+
+	return ret[0], ret[1], ret[2], ret[3], nil
+}
+
+func getWatchRepositories() Set {
+
+	ret := NewSet()
+	names := os.Getenv("WATCH_BRANCES")
+	if len(names) > 0 {
+		for _, currBranch := range strings.Split(names, ",") {
+			ret.Add(currBranch)
+		}
+	} else {
+		ret.Add("master")
+	}
+	log.Info("Watch branches:", watchBranches.ToArray())
+
+	return ret
 }
