2c0e7da4edb3314d0961aa2dd93272d356411269
Effi <effoeffi@gmail.com>
2017-02-19 12:03:04 +0200

    skip integration tests unless adding a dedicated env. var;

--- itest/integration_test.go
+++ itest/integration_test.go
@@ -4,9 +4,15 @@ import (
 	"testing"
 	"github.com/stretchr/testify/assert"
 	"net/http"
+	"os"
 )
 
 func TestETE(t *testing.T) {
+
+	const itest = "INTEGRATION_TESTS"
+	if os.Getenv(itest) != "true" {
+		t.Skipf("TestETE skiped. To run it, please, add enviroment varible: %s=true", itest)
+	}
 
 	res, err := SendEvent(Push, "nginx", "master-default-nginxdeployment-1.10")
 	assert.NoError(t, err)
